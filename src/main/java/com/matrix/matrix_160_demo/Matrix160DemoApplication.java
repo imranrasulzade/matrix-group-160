5cfb3070b35789d827b7afc535536724f2e60159
imranrasulzade <imranrasulzade5@gmail.com>
2024-08-12 19:11:00 +0400

    initial git

---
+++ src/main/java/com/matrix/matrix_160_demo/Matrix160DemoApplication.java
@@ -0,0 +1,12 @@
+package com.matrix.matrix_160_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Matrix160DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Matrix160DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/matrix/matrix_160_demo/Matrix160DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.matrix_160_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Matrix160DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
