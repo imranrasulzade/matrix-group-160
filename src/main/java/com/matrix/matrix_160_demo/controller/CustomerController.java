85f501779133674cb245b9b956edebbd235ffbb3
ughhrm <huseynovugur795@gmail.com>
2024-08-12 08:54:00 -0700

    ugur add

---
+++ src/main/java/com/matrix/matrix_160_demo/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.matrix.matrix_160_demo.controller;
+
+public class CustomerController {
+
+    public void doGet(){
+        for(int i=0; i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
