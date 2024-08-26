c7fc77c0d9309afc30415c24d705a9b0553031ae
memmedovnicatt <nicat.mammadov2022@khazar.org>
2024-08-26 14:29:33 +0400

    service added

--- src/main/java/com/matrix/matrix_160_demo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/matrix/matrix_160_demo/service/impl/EmployeeServiceImpl.java
@@ -1,13 +1,35 @@
 package com.matrix.matrix_160_demo.service.impl;
 
 
+import com.matrix.matrix_160_demo.entity.Employee;
+import com.matrix.matrix_160_demo.repositories.EmployeeRepository;
 import com.matrix.matrix_160_demo.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    @Override
+    public Optional<Employee> findById(Integer id) {
+        return employeeRepository.findById(id);
+    }
 
+    @Override
+    public Employee add(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 
+    @Override
+    public void deleteById(Integer id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
